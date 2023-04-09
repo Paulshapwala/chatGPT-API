eec2f14c0c8ac96706264ea52abcbe5cb691104a
Paulshapwala <www.wolverine99999@gmail.com>
2023-04-09 16:07:07 +0300

    added a function to format text

--- app.py
+++ app.py
@@ -3,12 +3,32 @@ import PySimpleGUI as sg
 import requests
 import json
 
+def split_length(string, line_length=17, limit=15, word_delimiter=' ', line_delimiter='\n'):
+    # string_list = string.copy()
+    initial = limit
+    i = 2
+    string_list = string.split(line_delimiter)
+    
+    for index, value in enumerate(string_list):
+        if len(value) >= 50:
+            new_list = string_list[index].split(word_delimiter)
+            while True:
+                if len(new_list[limit:]) >= line_length:
+                    new_list[limit] = f'{new_list[limit]}{line_delimiter}'
+                    limit = initial * i
+                    i += 1
+                string_list[index] = word_delimiter.join(new_list)
+                if len(new_list[limit:]) <= line_length:
+                    break
+    return line_delimiter.join(string_list)
+            
+
 def get_chat_response(message):
     # Set up the request parameters
     url = "https://api.openai.com/v1/chat/completions"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer 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"
+        "Authorization": "Bearer <YOUR_AUTH_TOKEN>"
     }
     payload = {
         "model": "gpt-3.5-turbo",
@@ -58,6 +78,6 @@ while True:
         if note:
             save_note(note)
             save_note(message)
-            save_note(response)
+            save_note(split_length(response))
 
 window.close()
